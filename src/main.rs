79b61b11753532dedd9fcfa9d19332211e1725df
Darshan Parajuli <parajulidarshan@gmail.com>
2024-11-29 22:29:09 -0800

    Return Result type
    Signed-off-by: Darshan Parajuli <parajulidarshan@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,27 @@ const TIMEOUT_ENV_VAR: &'static str = "GIT_WAIT_TIMEOUT_MS";
 
 fn main() {
     // Get current dir.
-    let dir = current_dir()
-        .unwrap_or_else(|_| report_error("Unable to read current directory.".to_string()));
+    let dir = current_dir().unwrap_or_else(|_| {
+        eprintln!("ERROR: unable to read current directory.");
+        std::process::exit(1);
+    });
 
+    if let Err(e) = run(&dir) {
+        eprintln!("ERROR: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run(current_dir: &Path) -> Result<(), String> {
+    // Get current dir.
     let mut args = std::env::args().collect::<Vec<_>>();
     args[0] = "git".to_string();
-
     // Find .git dir.
-    if let Some(git_dir) = find_git_directory(&dir) {
+    if let Some(git_dir) = find_git_directory(&current_dir) {
         let timeout = if let Ok(timeout) = std::env::var(TIMEOUT_ENV_VAR) {
-            let timeout = timeout.parse().unwrap_or_else(|e| {
-                report_error(format!("timeout parse error: {}", e));
-            });
+            let timeout = timeout
+                .parse()
+                .map_err(|e| format!("timeout parse error: {}", e))?;
             Some(Duration::from_millis(timeout))
         } else {
             None
@@ -35,16 +44,16 @@ fn main() {
 
         let index_lock_path = git_dir.join(INDEX_LOCK_NAME);
         if index_lock_path.exists() {
-            print!("Waiting on index.lock... ");
+            print!("waiting on index.lock... ");
             stdout().flush().unwrap();
-            wait(&index_lock_path, timeout);
+            wait(&index_lock_path, timeout)?;
             println!("done!");
-            run_git_cmd(&args);
+            run_git_cmd(&args)
         } else {
-            run_git_cmd(&args);
+            run_git_cmd(&args)
         }
     } else {
-        run_git_cmd(&args);
+        run_git_cmd(&args)
     }
 }
 
@@ -65,7 +74,7 @@ fn find_git_directory(dir: &Path) -> Option<PathBuf> {
     }
 }
 
-fn run_git_cmd(args: &[String]) {
+fn run_git_cmd(args: &[String]) -> Result<(), String> {
     // Unwrapping is fine here since the first arg is "git".
     let program_name = CString::new(args[0].as_bytes()).unwrap();
 
@@ -74,7 +83,7 @@ fn run_git_cmd(args: &[String]) {
         .into_iter()
         .map(|e| CString::new(e.as_bytes()))
         .collect::<Result<Vec<_>, _>>()
-        .unwrap_or_else(|e| report_error(format!("invalid arg string: {}", e)));
+        .map_err(|e| format!("invalid arg string: {}", e))?;
 
     // Convert args to `CString`s to vec of pointers.
     let mut arg_ptrs = args.iter().map(|e| e.as_ptr()).collect::<Vec<_>>();
@@ -83,11 +92,13 @@ fn run_git_cmd(args: &[String]) {
     // execvp only returns if there was an error.
     let result = unsafe { execvp(program_name.as_ptr(), arg_ptrs.as_ptr()) };
     if result == -1 {
-        report_error(format!("error executing git, code: {}", errno()));
+        Err(format!("error executing git, code: {}", errno()))
+    } else {
+        Ok(())
     }
 }
 
-fn wait(path: &Path, timeout: Option<Duration>) {
+fn wait(path: &Path, timeout: Option<Duration>) -> Result<(), String> {
     let (tx, rx) = mpsc::channel::<Event>();
 
     let mut watcher = RecommendedWatcher::new(
@@ -98,20 +109,16 @@ fn wait(path: &Path, timeout: Option<Duration>) {
         },
         Config::default(),
     )
-    .unwrap_or_else(|e| {
-        report_error(format!("Unable to initialize file watcher: {}", e));
-    });
+    .map_err(|e| format!("unable to initialize file watcher: {}", e))?;
 
     if let Err(e) = watcher.watch(path, RecursiveMode::NonRecursive) {
-        match e.kind {
+        return match e.kind {
             ErrorKind::PathNotFound => {
                 // index.lock no longer exists at this point.
-                return;
+                Ok(())
             }
-            _ => {
-                report_error(format!("Unable to watch index.lock: {}", e));
-            }
-        }
+            _ => Err(format!("nnable to watch index.lock: {}", e)),
+        };
     }
 
     loop {
@@ -119,27 +126,22 @@ fn wait(path: &Path, timeout: Option<Duration>) {
             match rx.recv_timeout(timeout) {
                 Ok(event) => {
                     if event.kind == EventKind::Remove(RemoveKind::File) {
-                        return;
+                        return Ok(());
                     }
                 }
                 Err(RecvTimeoutError::Timeout) => {
-                    report_error("timed out!".to_string());
+                    return Err("timed out!".to_string());
                 }
                 Err(RecvTimeoutError::Disconnected) => {
-                    report_error("broken channel".to_string());
+                    return Err("broken channel".to_string());
                 }
             }
         } else {
             for event in &rx {
                 if event.kind == EventKind::Remove(RemoveKind::File) {
-                    return;
+                    return Ok(());
                 }
             }
         }
     }
-}
-
-fn report_error(msg: String) -> ! {
-    eprintln!("ERROR: {}", msg);
-    std::process::exit(1)
 }
