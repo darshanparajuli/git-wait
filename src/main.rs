09e1675213c05b3df6b449ac8d55c591f9b4f77c
Darshan Parajuli <parajulidarshan@gmail.com>
2024-11-29 23:58:18 -0800

    Add tests
    Signed-off-by: Darshan Parajuli <parajulidarshan@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -15,32 +15,28 @@ const GIT_DIR_NAME: &'static str = ".git";
 const TIMEOUT_ENV_VAR: &'static str = "GIT_WAIT_TIMEOUT_MS";
 
 fn main() {
-    // Get current dir.
-    let dir = current_dir().unwrap_or_else(|_| {
-        eprintln!("ERROR: unable to read current directory.");
-        std::process::exit(1);
-    });
-
-    if let Err(e) = run(dir) {
-        eprintln!("ERROR: {}", e);
-        std::process::exit(1);
+    let args = std::env::args().collect::<Vec<_>>();
+    match maybe_wait_for_index_lock(args) {
+        Ok(args) => {
+            if let Err(e) = run_git_cmd(&args) {
+                eprintln!("ERROR: {}", e);
+                std::process::exit(1);
+            }
+        }
+        Err(e) => {
+            eprintln!("ERROR: {}", e);
+            std::process::exit(1);
+        }
     }
 }
 
-fn run(mut dir: PathBuf) -> Result<(), String> {
-    // Get current dir.
-    let mut args = std::env::args().collect::<Vec<_>>();
+fn maybe_wait_for_index_lock(mut args: Vec<String>) -> Result<Vec<String>, String> {
     args[0] = "git".to_string();
+
+    let mut dir = current_dir().map_err(|_| "unable to read current directory.".to_string())?;
     // Find .git dir.
     if traverse_to_git_dir(&mut dir) {
-        let timeout = if let Ok(timeout) = std::env::var(TIMEOUT_ENV_VAR) {
-            let timeout = timeout
-                .parse()
-                .map_err(|e| format!("timeout parse error: {}", e))?;
-            Some(Duration::from_millis(timeout))
-        } else {
-            None
-        };
+        let timeout = read_timeout_env_var()?;
 
         let index_lock_path = dir.join(INDEX_LOCK_NAME);
         if index_lock_path.exists() {
@@ -48,12 +44,23 @@ fn run(mut dir: PathBuf) -> Result<(), String> {
             stdout().flush().unwrap();
             wait(&index_lock_path, timeout)?;
             println!("done!");
-            run_git_cmd(&args)
+            Ok(args)
         } else {
-            run_git_cmd(&args)
+            Ok(args)
         }
     } else {
-        run_git_cmd(&args)
+        Err("git directory not found".to_string())
+    }
+}
+
+fn read_timeout_env_var() -> Result<Option<Duration>, String> {
+    if let Ok(timeout) = std::env::var(TIMEOUT_ENV_VAR) {
+        let timeout = timeout
+            .parse()
+            .map_err(|e| format!("timeout parse error: {}", e))?;
+        Ok(Some(Duration::from_millis(timeout)))
+    } else {
+        Ok(None)
     }
 }
 
@@ -141,6 +148,112 @@ fn wait(path: &Path, timeout: Option<Duration>) -> Result<(), String> {
                     return Ok(());
                 }
             }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::maybe_wait_for_index_lock;
+    use lazy_static::lazy_static;
+    use std::fs::File;
+    use std::path::{Path, PathBuf};
+    use std::sync::mpsc::RecvTimeoutError;
+    use std::sync::{mpsc, Mutex};
+    use std::time::Duration;
+    use std::{env, fs};
+
+    lazy_static! {
+        // This global mutex is used to ensure access to the test dir is only every done
+        // by one thread at a time for each test. Otherwise, multiple threads could try to
+        // create/delete the same files and error out.
+        static ref test_file_lock: Mutex<()> = Mutex::new(());
+    }
+
+    #[test]
+    fn repo_with_git_dir_is_valid() {
+        with_test_dir(|test_dir| {
+            fs::create_dir(&test_dir.path.join(".git")).unwrap();
+            assert!(
+                maybe_wait_for_index_lock(vec!["git".to_string(), "status".to_string()]).is_ok()
+            )
+        });
+    }
+
+    #[test]
+    fn repo_without_git_dir_is_invalid() {
+        with_test_dir(|_| {
+            assert!(
+                maybe_wait_for_index_lock(vec!["git".to_string(), "status".to_string()]).is_err()
+            );
+        });
+    }
+
+    #[test]
+    fn wait_if_index_lock_is_present() {
+        with_test_dir(|test_dir| {
+            fs::create_dir(&test_dir.path.join(".git")).unwrap();
+
+            // Create index.lock file.
+            let index_file = test_dir.path.join(".git/index.lock");
+            let _ = File::create(&index_file).unwrap();
+
+            // Spawn a new thread to wait on the index.lock file and send the result in the
+            // channel once done waiting.
+            let (tx, rx) = mpsc::channel::<Result<Vec<String>, String>>();
+            let handle = std::thread::spawn(move || {
+                let result =
+                    maybe_wait_for_index_lock(vec!["git".to_string(), "status".to_string()]);
+                tx.send(result).unwrap();
+            });
+
+            // Wait 100ms. It should time out since index.lock file is still present.
+            let result = rx.recv_timeout(Duration::from_millis(100));
+            assert_eq!(result, Err(RecvTimeoutError::Timeout));
+
+            fs::remove_file(index_file).unwrap();
+
+            // Give enough time for the file to be deleted.
+            let result = rx.recv_timeout(Duration::from_millis(200));
+            assert!(result.unwrap().is_ok());
+
+            handle.join().unwrap();
+        });
+    }
+
+    fn with_test_dir(block: fn(&TestDir) -> ()) {
+        let _lock = test_file_lock.lock().unwrap();
+        let test_dir = TestDir::new();
+        block(&test_dir);
+    }
+
+    struct TestDir {
+        current_dir: PathBuf,
+        path: Box<Path>,
+    }
+
+    impl TestDir {
+        fn new() -> Self {
+            let temp_dir = env::temp_dir().join("git-wait-test-dir");
+            if temp_dir.exists() {
+                fs::remove_dir_all(&temp_dir).unwrap();
+            }
+
+            fs::create_dir(&temp_dir).unwrap();
+
+            let current_dir = env::current_dir().unwrap();
+            env::set_current_dir(&temp_dir).unwrap();
+            Self {
+                path: temp_dir.into_boxed_path(),
+                current_dir,
+            }
+        }
+    }
+
+    impl Drop for TestDir {
+        fn drop(&mut self) {
+            env::set_current_dir(&self.current_dir).unwrap();
+            fs::remove_dir_all(&self.path).unwrap();
         }
     }
 }
